39a44f2b6503cdf66a59f0aca03e27f4ef626f95
Štěpán Konečný <stepan.konecny@cookielab.io>
2026-02-17 19:25:37 +0100

    Add optional bucket/report creation and SNS support to source module
    - Add create_bucket toggle to skip S3 bucket creation for existing buckets
    - Add create_report toggle to skip CUR report definition for existing reports
    - Add use_sns option for SNS-based notifications instead of direct Lambda
    - Add sns_topic_name, sns_subscriber_arns, cur_s3_prefix variables
    - Add sns_topic_arn output
    - Update README with new inputs/outputs and usage example

--- modules/source/outputs.tf
+++ modules/source/outputs.tf
@@ -1,29 +1,34 @@
 output "bucket_id" {
   description = "ID of the CUR S3 bucket"
-  value       = module.cur_bucket.s3_bucket_id
+  value       = local.bucket_id
 }
 
 output "bucket_arn" {
   description = "ARN of the CUR S3 bucket"
-  value       = module.cur_bucket.s3_bucket_arn
+  value       = local.bucket_arn
 }
 
 output "bucket_name" {
   description = "Name of the CUR S3 bucket"
-  value       = module.cur_bucket.s3_bucket_id
+  value       = local.bucket_name
 }
 
 output "cur_report_name" {
   description = "Name of the CUR report"
-  value       = aws_cur_report_definition.this.report_name
+  value       = var.create_report ? aws_cur_report_definition.this[0].report_name : null
 }
 
 output "cur_prefix" {
   description = "S3 prefix where CUR reports are stored"
-  value       = aws_cur_report_definition.this.s3_prefix
+  value       = local.cur_s3_prefix
 }
 
 output "account_id" {
   description = "AWS Account ID where this module is deployed"
   value       = data.aws_caller_identity.current.account_id
+}
+
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic for CUR notifications (null if use_sns = false)"
+  value       = var.use_sns ? aws_sns_topic.cur[0].arn : null
 }
